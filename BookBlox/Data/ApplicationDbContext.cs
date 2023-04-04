b81691b77d292a1769218910dc9e9e506c761b1f
Saurabh Kumar <84146288+mahaguru-codes@users.noreply.github.com>
2023-04-04 17:29:58 +0530

    First Commit By Mahaguru
    1. Created the project --> ASP.NET Core 6.0 MVC
    2. Installed packages for entity framework --> (Entity framework core, Entity framework sql server, Entity framework tools)
    3. Added DbContext class and created setup for connection string in appsettings.json
    4. Added migration
    5. Created table as same as the class in code using package manager console

---
+++ BookBlox/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookBlox.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookBlox.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BookBlox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookBlox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
